a29bede9feb6d4f1f5a5079fb605f0a3b4409352
Eason <1093464073@qq.com>
2023-05-11 09:07:05 +0800

    OK，正式结束Javase的学习

--- src/com/gdby/myproject/annotation/Text.java
+++ src/com/gdby/myproject/annotation/Text.java
@@ -1,7 +1,10 @@
 package com.gdby.myproject.annotation;
 
+@MyAnnotation(year = "20")
 public class Text {
     public static void main(String[] args) {
+        // deprecated这个关键字表示过时方法
+        // new Man().doSome();
         
     }
 }
